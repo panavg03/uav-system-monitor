031902a4f020c667d05cfdb387472de25ffb0459
Prithvi <prithvirajsingh.dev@gmail.com>
2026-09-07 15:40:46 +0530

    structure

---
+++ back-end/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "UAV Engine Backend is running 🚀"
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
